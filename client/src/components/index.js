b647b5a80d64768b22c42bf4897824e8cbf1d672
NDOULOU Jean <jndoulou@g-a102b-23.local>
2018-11-28 18:15:12 +0100

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Mobile } from './Mobile'
+export { default as Desktop } from './Desktop'
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const isDeveloping = process.env.NODE_ENV !== 'production'
+const port = isDeveloping ? 5000 : process.env.PORT
+const io = require('socket.io').listen(app.listen(port))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+if (isDeveloping) {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+
+  io.sockets.on('connection', function response(socket) {
+    console.log('connected client')
+  })
+}
+
+console.log('server is running on http://localhost:' + port)
