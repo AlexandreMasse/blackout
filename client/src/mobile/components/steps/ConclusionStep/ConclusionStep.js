c387762e9b7530b50f60698061bcd347d5fdcaa2
Jean <ndoulou.jean@gmail.com>
2019-02-15 01:08:00 +0100

    Conclusion number

--- client/src/mobile/components/steps/ConclusionStep/ConclusionStep.js
+++ client/src/mobile/components/steps/ConclusionStep/ConclusionStep.js
@@ -140,55 +140,70 @@ class ConclusionStep extends Component {
         <div className="conclusion-step__cards">
           <div className="conclusion-step__cards__card">
             <p>Aujourd'hui en France, la loi empêche les opérateurs de bloquer l'accès à certains contenus sur internet. </p>
+            <span className="conclusion-step__cards__number">1/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Elle garantit à toute personne une utilisation des réseaux numériques libre, égale et sans discrimination.
             </p>
+            <span className="conclusion-step__cards__number">2/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Ce principe, inexistant en Chine, est aussi remis en question dans certains pays occidentaux.</p>
+            <span className="conclusion-step__cards__number">3/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Sa disparition entraînerait la mise en place d’un réseau à deux vitesse où payer plus permet un meilleur accès.</p>
+            <span className="conclusion-step__cards__number">4/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>“Ce serait la création d’un internet pour les riches et un internet pour les pauvres.”</p>
             <p className={"source"}>→ Mounir Mahjoubi,<br/><span>Secrétaire d’état chargé du numérique,<br/> 7 septembre 2018</span></p>
+            <span className="conclusion-step__cards__number">5/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Aux États-Unis, la neutralité du net a été supprimée le 11 juin 2018, contre l’avis de 86% des américains.</p>
             <p className={"source"}><span>www.aclu.org<br/>6 juin 2018</span></p>
+            <span className="conclusion-step__cards__number">6/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Depuis, presque tous les opérateurs brident le débit de leurs utilisateurs sur Netflix et Youtube.</p>
             <p className={"source"}><span>https://dd.meddle.mobi/USStats.html,<br/>11 novembre 2018</span></p>
+            <span className="conclusion-step__cards__number">7/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Cette information à priori anodine démontre pourtant le pouvoir de censurer n'importe quel contenu.</p>
+            <span className="conclusion-step__cards__number">8/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>C’est un risque considérable car aujourd’hui internet est un facteur majeur d’intégration sociale.</p>
+            <span className="conclusion-step__cards__number">9/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Par exemple, 74% des demandeurs d’emploi utilisent internet pour trouver du travail.</p>
             <p className={"source"}><span>Baromètre CRÉDOC,<br/>juin 2018</span></p>
+            <span className="conclusion-step__cards__number">10/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>C’est aussi un bien nécessaire à la vie quotidienne :</p>
+            <span className="conclusion-step__cards__number">10/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Une personne sur deux y fait des recherches en rapport à sa santé</p>
             <p className={"source"}><span>Baromètre CRÉDOC,<br/>juin 2018</span></p>
+            <span className="conclusion-step__cards__number">11/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Et deux tiers des français font leurs démarches administratives en ligne.</p>
             <p className={"source"}><span>Baromètre CRÉDOC,<br/>juin 2018</span></p>
+            <span className="conclusion-step__cards__number">12/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>En bref, Internet est un (potentiel) outil de lutte contre les inégalités.</p>
+            <span className="conclusion-step__cards__number">13/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <p>Pour garantir ce rôle, la neutralité du net est indispensable.</p>
+            <span className="conclusion-step__cards__number">14/15</span>
           </div>
           <div className="conclusion-step__cards__card">
             <div className="wrap">
@@ -197,6 +212,7 @@ class ConclusionStep extends Component {
                   <p>www.laquadrature.net</p>
                 </a>
             </div>
+            <span className="conclusion-step__cards__number">15/15</span>
           </div>
         </div>
       </div>
