7abf2d8a588f12074f4ffc9930644248ea39640b
Jean <ndoulou.jean@gmail.com>
2019-04-26 00:21:26 +0200

    add sound off flashlight

--- client/src/mobile/assets/asset-list.js
+++ client/src/mobile/assets/asset-list.js
@@ -5,6 +5,7 @@ import logotype from './video/logotype.mp4'
 import trame from './img/trame.jpg'
 import notification from './sounds/notification.mp3'
 import mobileFlash from './sounds/mobileFlash.mp3'
+import flashOff from './sounds/off.mp3'
 import mobileUp from './sounds/mobileUp.mp3'
 import mobileDown from './sounds/mobileDown.mp3'
 import fingerprintDisadvandtage from './sounds/fingerprintDisadvandtage.mp3'
@@ -14,18 +15,19 @@ import sendNotif from './sounds/sendNotif.mp3'
 import codeSound from './sounds/codeSound.mp3'
 
 export const assetsToLoad = {
-    arrowDonwWhite: {url: arrowDonwWhite, name: "arrowDonwWhite", type:"image"},
-    arrowDonwBlack: {url: arrowDonwBlack, name: "arrowDonwBlack", type:"image"},
-    map: {url:map, name: "map", type:"image"},
-    trame: {url:trame, name: "trame", type:"image"},
-    logotype: {url:logotype, name: "logotype", type:"video"},
-    notification: {url:notification, name: "notification", type:"sound"},
-    mobileFlash: {url:mobileFlash, name: "mobileFlash", type:"sound"},
-    mobileUp: {url:mobileUp, name: "mobileUp", type:"sound"},
-    mobileDown: {url:mobileDown, name: "mobileDown", type:"sound"},
-    fingerprintDisadvandtage: {url:fingerprintDisadvandtage, name: "fingerprintDisadvandtage", type:"sound"},
-    fingerprintAdvandtage: {url:fingerprintAdvandtage, name: "fingerprintAdvandtage", type:"sound"},
-    sliderEnd: {url:sliderEnd, name: "sliderEnd", type:"sound"},
-    sendNotif: {url:sendNotif, name: "sendNotif", type:"sound"},
-    codeSound: {url:codeSound, name: "codeSound", type:"sound"},
+  arrowDonwWhite: { url: arrowDonwWhite, name: 'arrowDonwWhite', type: 'image' },
+  arrowDonwBlack: { url: arrowDonwBlack, name: 'arrowDonwBlack', type: 'image' },
+  map: { url: map, name: 'map', type: 'image' },
+  trame: { url: trame, name: 'trame', type: 'image' },
+  logotype: { url: logotype, name: 'logotype', type: 'video' },
+  notification: { url: notification, name: 'notification', type: 'sound' },
+  mobileFlash: { url: mobileFlash, name: 'mobileFlash', type: 'sound' },
+  flashOff: { url: flashOff, name: 'flashOff', type: 'sound' },
+  mobileUp: { url: mobileUp, name: 'mobileUp', type: 'sound' },
+  mobileDown: { url: mobileDown, name: 'mobileDown', type: 'sound' },
+  fingerprintDisadvandtage: { url: fingerprintDisadvandtage, name: 'fingerprintDisadvandtage', type: 'sound' },
+  fingerprintAdvandtage: { url: fingerprintAdvandtage, name: 'fingerprintAdvandtage', type: 'sound' },
+  sliderEnd: { url: sliderEnd, name: 'sliderEnd', type: 'sound' },
+  sendNotif: { url: sendNotif, name: 'sendNotif', type: 'sound' },
+  codeSound: { url: codeSound, name: 'codeSound', type: 'sound' }
 }
--- client/src/mobile/components/components/withDeviceOrientation/withDeviceOrientation.js
+++ client/src/mobile/components/components/withDeviceOrientation/withDeviceOrientation.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { AssetsManager } from './../../../../managers';
-import { Howl } from 'howler';
+import React from 'react'
+import { AssetsManager } from './../../../../managers'
+import { Howl } from 'howler'
 const withDeviceOrientation = WrappedComponent => {
   class HOC extends React.Component {
     constructor(props) {
-      super(props);
-      this.initFlashlightSound();
+      super(props)
+      this.initFlashlightSound()
       // The angle at which we stop listening for input from the phone's gyro.
-      this.MAX_X_ANGLE = 20;
-      this.MAX_Y_ANGLE = 24;
+      this.MAX_X_ANGLE = 20
+      this.MAX_Y_ANGLE = 24
 
-      this.loopUpdateTimer = 0;
-      this.position = { x: 0, y: 0 };
-      this.latestAlpha = 0;
-      this.baseAlpha = null;
-      this.touching = false;
+      this.loopUpdateTimer = 0
+      this.position = { x: 0, y: 0 }
+      this.latestAlpha = 0
+      this.baseAlpha = null
+      this.touching = false
 
       this.state = {
         positionListened: this.position,
         isLightOn: false
-      };
+      }
     }
     initFlashlightSound = () => {
-      const mobileFlashAsset = AssetsManager.get('mobileFlash');
+      const mobileFlashAsset = AssetsManager.get('mobileFlash')
       this.mobileFlash = new Howl({
         src: mobileFlashAsset.src,
         volume: 1,
@@ -31,77 +31,85 @@ const withDeviceOrientation = WrappedComponent => {
         autoplay: false,
         loop: false,
         format: ['mp3']
-      });
-    };
+      })
+      const mobileFlashOffAsset = AssetsManager.get('flashOff')
+      this.mobileFlashOff = new Howl({
+        src: mobileFlashOffAsset.src,
+        volume: 1,
+        html5: true,
+        preload: true,
+        autoplay: false,
+        loop: false,
+        format: ['mp3']
+      })
+    }
 
     // Convert the phone's gyro data into screen coordinates.
     handleDeviceOrientation = data => {
       let x,
         y,
         alpha = (this.latestAlpha = data.alpha),
-        beta = data.beta;
+        beta = data.beta
 
       if (this.baseAlpha !== null) {
-        alpha = alpha - this.baseAlpha;
-        alpha += 360;
-        alpha %= 360;
+        alpha = alpha - this.baseAlpha
+        alpha += 360
+        alpha %= 360
       }
 
       // Left/right rotation.
       if (alpha > 360 - this.MAX_X_ANGLE) {
         // phone is rotating right:
-        x = (100 / this.MAX_X_ANGLE) * (360 - alpha);
+        x = (100 / this.MAX_X_ANGLE) * (360 - alpha)
       } else if (alpha < this.MAX_X_ANGLE) {
         // phone is rotating left:
-        x = (100 / this.MAX_X_ANGLE) * (0 - alpha);
+        x = (100 / this.MAX_X_ANGLE) * (0 - alpha)
       } else {
         // Stop rotation at max angle.
         if (alpha > this.MAX_X_ANGLE && alpha < 180) {
-          x = -100;
+          x = -100
         } else {
-          x = 100;
+          x = 100
         }
       }
 
       // Up/down rotation.
-      if (
-        (beta > 0 && beta <= this.MAX_Y_ANGLE) ||
-        (beta < 0 && beta >= this.MAX_Y_ANGLE * -1)
-      ) {
-        y = (100 / this.MAX_Y_ANGLE) * (beta * -1);
+      if ((beta > 0 && beta <= this.MAX_Y_ANGLE) || (beta < 0 && beta >= this.MAX_Y_ANGLE * -1)) {
+        y = (100 / this.MAX_Y_ANGLE) * (beta * -1)
       } else {
         if (beta > 0) {
-          y = -100;
+          y = -100
         } else {
-          y = 100;
+          y = 100
         }
       }
 
       // Normalize percentages to from (0, 100) to (0, 1):
-      x *= 0.01;
-      y *= 0.01;
+      x *= 0.01
+      y *= 0.01
 
-      this.position.x = x;
-      this.position.y = y;
-    };
+      this.position.x = x
+      this.position.y = y
+    }
 
     handleTouchStartEvent = e => {
-      e.preventDefault();
+      e.preventDefault()
       e.target.parentNode.classList.add('power-button--active')
-      this.setState({isLightOn : true})
+      this.setState({ isLightOn: true })
       this.mobileFlash.play()
       // Every time we touch and hold we calibrate to the center of the screen.
-      this.baseAlpha = this.latestAlpha;
-      this.touching = true;
-      this.update();
-    };
+      this.baseAlpha = this.latestAlpha
+      this.touching = true
+      this.update()
+    }
 
     handleTouchEndEvent = e => {
-      e.target.parentNode.classList.remove('power-button--active');
-      this.setState({isLightOn : false})
-      e.preventDefault();
-      this.touching = false;
-    };
+      e.target.parentNode.classList.remove('power-button--active')
+      this.setState({ isLightOn: false })
+      this.mobileFlashOff.play()
+      e.preventDefault()
+      this.touching = false
+    }
 
     update = () => {
       if (this.touching) {
@@ -109,54 +117,46 @@ const withDeviceOrientation = WrappedComponent => {
         // io.emit('position', this.position);
         this.setState({
           positionListened: { x: this.position.x, y: this.position.y }
-        });
-        this.loopUpdateTimer = setTimeout(this.update, 16);
+        })
+        this.loopUpdateTimer = setTimeout(this.update, 16)
       }
-    };
+    }
 
     initEventListener = wrappedComponentRef => {
-      const touchElement = wrappedComponentRef.querySelector('.power-button');
+      const touchElement = wrappedComponentRef.querySelector('.power-button')
       if (window.DeviceOrientationEvent) {
-        window.addEventListener(
-          'deviceorientation',
-          this.handleDeviceOrientation,
-          false
-        );
+        window.addEventListener('deviceorientation', this.handleDeviceOrientation, false)
 
-        touchElement.addEventListener(
-          'touchstart',
-          this.handleTouchStartEvent,
-          {
-            capture: true,
-            passive: false
-          }
-        );
+        touchElement.addEventListener('touchstart', this.handleTouchStartEvent, {
+          capture: true,
+          passive: false
+        })
 
         touchElement.addEventListener('mousedown', this.handleTouchStartEvent, {
           capture: true,
           passive: false
-        });
+        })
 
         touchElement.addEventListener('touchend', this.handleTouchEndEvent, {
           capture: true,
           passive: false
-        });
+        })
 
         touchElement.addEventListener('mouseup', this.handleTouchEndEvent, {
           capture: true,
           passive: false
-        });
+        })
       } else {
-        alert('This device is not supported.');
+        alert('This device is not supported.')
       }
-    };
+    }
 
     // We need to check for DeviceOrientation support because some devices do not
     // support it. For example, phones with no gyro.
     componentWillMount() {}
 
     shouldComponentUpdate() {
-      return this.touching;
+      return this.touching
     }
 
     render() {
@@ -167,11 +167,11 @@ const withDeviceOrientation = WrappedComponent => {
           isLightOn={this.state.isLightOn}
           handleRef={this.initEventListener}
         />
-      );
+      )
     }
   }
 
-  return HOC;
-};
+  return HOC
+}
 
-export default withDeviceOrientation;
+export default withDeviceOrientation
